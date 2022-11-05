bc1364fc3a47c99063f86d89b5d9284e186b87ab
Amit <joe.amit@yahoo.com>
2022-11-05 22:42:10 +0530

    You are given an array of integers, , denoting the marks scored by students in a class.

---
+++ HR-5.c
@@ -0,0 +1,56 @@
+/*
+You are given an array of integers, , denoting the marks scored by students in a class.
+
+The alternating elements , ,  and so on denote the marks of boys.
+Similarly, , ,  and so on denote the marks of girls.
+The array name, , works as a pointer which stores the base address of that array. In other words,  contains the address where  is stored in the memory.
+
+*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int marks_summation(int *, int, char);
+int marks_summation(int *marks, int number_of_students, char gender)
+{
+    int sum = 0;
+    char g;
+    if (103 == gender)
+    {
+        for (int i = 1; i < number_of_students; i += 2)
+        {
+            sum = sum + marks[i];
+        }
+        return sum;
+    }
+    else
+    {
+        for (int i = 0; i < number_of_students; i += 2)
+        {
+            sum = sum + marks[i];
+        }
+        return sum;
+    }
+}
+
+int main()
+{
+    int number_of_students;
+    char gender;
+    int sum;
+
+    scanf("%d", &number_of_students);
+    int *marks = (int *)malloc(number_of_students * sizeof(int));
+
+    for (int student = 0; student < number_of_students; student++)
+    {
+        scanf("%d", (marks + student));
+    }
+
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+
+    return 0;
+}
